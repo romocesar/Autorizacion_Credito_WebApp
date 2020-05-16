62a61a93bec57020d9f35e155c75b2fe7f3106eb
Ignacio <ignacioggb@hotmail.com>
2020-05-15 18:00:43 -0700

    Handlebars MVC

--- server.js
+++ server.js
@@ -1,55 +1,152 @@
-//
-var express = require("express");
+var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var exphbs = require('express-handlebars');
-var mysql = require('mysql');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var User = require('./models/user');
+var hbs = require('express-handlebars'); 
+var path = require('path'); 
 
+
+// invoke an instance of express application.
 var app = express();
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(methodOverride('_method'));
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
-app.set('view engine','handlebars');
-var port = 3000;
-app.listen(port);
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Xacn#1855',
-    database: 'movie_planner_DB'
+// set our application port
+app.set('port', 9000);
+
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
+
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+// handle bars config
+app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); 
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'hbs'); 
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
 });
 
-connection.connect(function(err){
-    if(err)throw err;
-    console.log('connected as id ' + connection.threadId);
-})
+var hbsContent = {userName: '', loggedin: false, title: "You are not logged in today", body: "Hello World"}; 
 
-app.get('/',function(req,res){
-    connection.query('SELECT * FROM movies;',function(err,data){
-        res.render('index',{movies:data})
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+		
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }    
+};
+
+
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+// route for user signup
+app.route('/signup')
+    //.get(sessionChecker, (req, res) => {
+    .get((req, res) => {
+        //res.sendFile(__dirname + '/public/signup.html');
+        res.render('signup', hbsContent);
     })
-})
-
-app.post('/create', function(req,res){
-    connection.query('INSERT INTO movies (movie) VALUES (?);', [
-        req.body.movie], function(err,result){
-            if(err)throw err;
-            res.redirect('/');
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            //email: req.body.email,
+            password: req.body.password
         })
-})
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
 
-app.put('/update',function(req,res){
-    connection.query('UPDATE movies SET movie = ? WHERE id = ?;',[
-        req.body.movie, req.body.id], function(err,results){
-        res.redirect('/');
-    })
-})
 
-app.delete('/delete',function(req,res){
-    connection.query("DELETE FROM movies WHERE id = ?;",[req.body.id],
-    function(err,results){
-        if(err) throw err;
-        res.redirect('/');
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        //res.sendFile(__dirname + '/public/login.html');
+        res.render('login', hbsContent);
     })
-})
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+		hbsContent.loggedin = true; 
+		hbsContent.userName = req.session.user.username; 
+		//console.log(JSON.stringify(req.session.user)); 
+		console.log(req.session.user.username); 
+		hbsContent.title = "You are logged in"; 
+        //res.sendFile(__dirname + '/public/dashboard.html');
+        res.render('index', hbsContent);
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+		hbsContent.loggedin = false; 
+		hbsContent.title = "You are logged out!"; 
+        res.clearCookie('user_sid');
+		console.log(JSON.stringify(hbsContent)); 
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for handling 404 requests(unavailable routes)
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+
+// start the express server
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
