261f8bb558a3231faa9f03ae9d03ee8b786ad884
Ignacio <ignacioggb@hotmail.com>
2020-05-23 22:48:59 -0700

    changed to react

--- models/user.js
+++ models/user.js
@@ -1,54 +1,67 @@
-var Sequelize = require('sequelize');
-var bcrypt = require('bcrypt');
-	
-// create a sequelize instance with our local postgres database information.
-const sequelize = new Sequelize('r8uuqfh5bjtkypxw', 'n43jp8wpwb0od8os', 'yfpqvbycpkqr795k', {
-	host:'pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	port: 3306,
-    dialect:'mysql',
-	pool:{
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000
-	}, 
-	operatorsAliases: false
-});
+const connection = require('../config/connection')// import the connection from the config to the database to make db queries
 
-// setup User model and its fields.
-var User = sequelize.define('users', {
-    id: {
-        type: Sequelize.INTEGER,
-        unique: true,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-	username: {
-        type: Sequelize.STRING,
-        unique: true,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-}); 
-
-User.beforeCreate((user, options) => {
-	const salt = bcrypt.genSaltSync();
-	user.password = bcrypt.hashSync(user.password, salt);
-});
-  
- 
-User.prototype.validPassword = function(password) {
-        return bcrypt.compareSync(password, this.password);
-      }; 
-
-// create all the defined tables in the specified database.
-sequelize.sync()
-    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
-    .catch(error => console.log('This error occured', error));
-
-// export User model for use in other files.
-module.exports = User;
+// Build a user Model to export to the controllers
+const User = {
+  selectAll: (cb) => {
+    const queryString = 'SELECT u.user_id, u.username, u.access_id, a.type FROM users AS u INNER JOIN access_levels AS a ON u.access_id=a.access_id ORDER BY u.user_id ASC;'
+    connection.query(queryString, (err, results) => {
+      if (err) throw err
+      cb(results)
+    })
+  },
+  getUserByUsernameWithPassword: (username, done) => {
+    const queryString = 'SELECT u.user_id, u.username,u.password, u.access_id, a.type FROM users AS u INNER JOIN access_levels AS a ON u.access_id=a.access_id WHERE username=? LIMIT 1;'
+    connection.execute(queryString, [username], (err, user) => {
+      if (err) {
+        return done(err, user)
+      }
+      return done(null, user[0])
+    })
+  },
+  getUserById: (id, done) => {
+    const queryString = 'SELECT u.user_id, u.username, u.access_id, a.type FROM users AS u INNER JOIN access_levels AS a ON u.access_id=a.access_id WHERE user_id=? LIMIT 1;'
+    connection.execute(queryString, [id], (err, user) => {
+      if (err) {
+        return done(err, user)
+      }
+      return done(null, user[0])
+    })
+  },
+  selectOneById: (id, cb) => {
+    const queryString = 'SELECT u.user_id, u.username, u.access_id, a.type FROM users AS u INNER JOIN access_levels AS a ON u.access_id=a.access_id WHERE user_id=? LIMIT 1;'
+    connection.execute(queryString, [id], (err, results) => {
+      if (err) throw err
+      cb(results)
+    })
+  },
+  selectOneByUsername: (username, cb) => {
+    const queryString = 'SELECT u.user_id, u.username, u.access_id, a.type FROM users AS u INNER JOIN access_levels AS a ON u.access_id=a.access_id WHERE username=? LIMIT 1;'
+    connection.execute(queryString, [username], (err, results) => {
+      if (err) throw err
+      cb(results)
+    })
+  },
+  deleteOne: (id, cb) => {
+    const queryString = 'DELETE FROM users WHERE user_id=?;'
+    connection.execute(queryString, [id], (err, result) => {
+      if (err) throw err
+      cb(result)
+    })
+  },
+  insertOne: (vals, cb) => {
+    const queryString = 'INSERT INTO users (username, password, access_id) VALUES (?,?,?)'
+    connection.execute(queryString, vals, (err, result) => {
+      if (err) throw err
+      cb(result)
+    })
+  },
+  updateOne: (vals, id, cb) => {
+    vals.push(id)
+    const queryString = 'UPDATE users SET username=?, password=?, access_id=? WHERE user_id=?;'
+    connection.execute(queryString, vals, (err, result) => {
+      if (err) throw err
+      cb(result)
+    })
+  }
+}
+module.exports = User
